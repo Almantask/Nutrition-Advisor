4494a1dbeb2b997fd95eaa9fed21a5bb7940179d
AlmantasK <almantusk@gmail.com>
2023-10-12 21:27:44 +0300

    feat: recommended kcal intake calculation

---
+++ src/Nutrition-Advisor.ConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿using Nutrition_Advisor;
+
+var recommendedKcal = RecommendedKcalIntakeCalculator.Calculate(Gender.Male, 80, 180, 30, ActivityLevel.Sedentary, Goal.BecomeFit);
+Console.WriteLine(recommendedKcal);
---
+++ src/Nutrition-Advisor/RecommendedKcalIntakeCalculator.cs
@@ -0,0 +1,89 @@
+﻿namespace Nutrition_Advisor
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public enum ActivityLevel
+    {
+        Sedentary = 1,
+        LightlyActive,
+        ModeratelyActive,
+        VeryActive,
+        SuperActive
+    }
+
+    public enum Goal
+    {
+        LoseWeight,
+        GainWeight,
+        BecomeFit
+    }
+
+    public static class RecommendedKcalIntakeCalculator
+    {
+        public static float Calculate(Gender gender, float weight, float height, int age, ActivityLevel activityLevel, Goal goal)
+        {
+            var tdee = CalculateTdee(gender, weight, height, age, activityLevel);
+            if(goal == Goal.LoseWeight)
+            {
+                return tdee - 500;
+            }
+            else if (goal == Goal.GainWeight)
+            {
+                return tdee + 250;
+            }
+            else
+            {
+                return tdee;
+            }
+        }
+
+        private static float CalculateTdee(Gender gender, float weight, float height, int age, ActivityLevel activityLevel)
+        {
+            float bmr = (gender == Gender.Male) ? CalculateBmrForMen(weight, height, age) : CalculateBmrForWomen(weight, height, age);
+            float tdee = CalculateTdee(bmr, activityLevel);
+            return tdee;
+        }
+
+        // Harris-Benedict equation for men
+        private static float CalculateBmrForMen(float weight, float height, int age)
+        {
+            return 88.362f + (13.397f * weight) + (4.799f * height) - (5.677f * age);
+        }
+
+        // Harris-Benedict equation for women
+        private static float CalculateBmrForWomen(float weight, float height, int age)
+        {
+            return 447.593f + (9.247f * weight) + (3.098f * height) - (4.330f * age);
+        }
+
+        private static float CalculateTdee(float bmr, ActivityLevel activityLevel)
+        {
+            float tdee = 0;
+
+            switch (activityLevel)
+            {
+                case ActivityLevel.Sedentary:
+                    tdee = bmr * 1.2f;
+                    break;
+                case ActivityLevel.LightlyActive:
+                    tdee = bmr * 1.375f;
+                    break;
+                case ActivityLevel.ModeratelyActive:
+                    tdee = bmr * 1.55f;
+                    break;
+                case ActivityLevel.VeryActive:
+                    tdee = bmr * 1.725f;
+                    break;
+                case ActivityLevel.SuperActive:
+                    tdee = bmr * 1.9f;
+                    break;
+            }
+
+            return tdee;
+        }
+    }
+}
